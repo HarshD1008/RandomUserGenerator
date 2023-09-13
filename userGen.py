1a99ad33a13f3ff02604e4ee47b353517119a416
HarshD1008 <144258259+HarshD1008@users.noreply.github.com>
2023-09-13 16:19:29 +0530

    Update userGen.py

--- userGen.py
+++ userGen.py
@@ -9,7 +9,7 @@ logging.basicConfig(
     format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.INFO
 )
 
-bottoken = None
+bottoken = '6418445156:AAHNyV-UoqvRwwEvqijQbGraJUae9ULLGGk'
 # start the bot
 print("Starting...")
 apiid = 6
